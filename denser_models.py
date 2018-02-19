9e7d64997775ab6e243cd41bb71c19c706a7124a
Filipe Assunção <fillassuncao@gmail.com>
2018-02-19 18:48:59 +0000

    multiple datasets

--- denser_models.py
+++ denser_models.py
@@ -6,10 +6,19 @@ from keras.models import load_model
 import numpy as np
 from sklearn.metrics import accuracy_score
 
+import sys
+import getopt
+import keras
+from keras import backend
+from keras.models import load_model
+import numpy as np
+from sklearn.metrics import accuracy_score
+import fashion_mnist
+
 def augmentation(x):
     pad_size = 4
 
-    h, w, c = 32, 32, 3
+    h, w, c = WIDTH, HEIGHT, NUM_CHANNELS
     pad_h = h + 2 * pad_size
     pad_w = w + 2 * pad_size
 
@@ -29,11 +38,19 @@ def augmentation(x):
     return aug_data
 
 
-def load_cifar(n_classes=100, test_size=7500):
-    if n_classes == 10:
+def load_cifar():
+    if DATASET == 'cifar-10':
         (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
-    elif n_classes == 100:
+    elif DATASET == 'cifar-100':
         (x_train, y_train), (x_test, y_test) = keras.datasets.cifar100.load_data()
+    elif DATASET == 'fashion-mnist':
+        (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+        x_train = np.reshape(x_train, (-1, 28, 28, 1))
+        x_test = np.reshape(x_test, (-1, 28, 28, 1))
+    elif DATASET == 'mnist':
+        (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+        x_train = np.reshape(x_train, (-1, 28, 28, 1))
+        x_test = np.reshape(x_test, (-1, 28, 28, 1))
 
     x_train = x_train.astype('float32')
     x_test = x_test.astype('float32')
@@ -47,38 +64,38 @@ def load_cifar(n_classes=100, test_size=7500):
     x_train -= x_mean
     x_test -= x_mean
 
-    y_train = keras.utils.to_categorical(y_train, n_classes)
-    y_test = keras.utils.to_categorical(y_test, n_classes)
+    y_train = keras.utils.to_categorical(y_train, NUM_CLASSES)
+    y_test = keras.utils.to_categorical(y_test, NUM_CLASSES)
 
     dataset = {'x_train': x_train, 'y_train': y_train,
                 'x_test': x_test, 'y_test': y_test}
 
     return dataset
 
-def test(n_classes=10, one_model=True):
+def test(one_model=True):
     
     models = []
     predictions = []
 
-    dataset = load_cifar(n_classes)
+    dataset = load_cifar()
 
-    if n_classes == 10:
+    if one_model:
         for train_idx in xrange(NUM_TRAINS):
-            models.append(load_model("%s/%s" % (CIFAR_10_DIR, TRAIN_FILENAME % train_idx), custom_objects={"backend": backend}))
+            print 'loading', train_idx
+            models.append(load_model("%s/net_1/%s" % (TRAIN_DIR, TRAIN_FILENAME % train_idx), custom_objects={"backend": backend}))
 
-    elif n_classes == 100:
-        if one_model:
-            for train_idx in xrange(NUM_TRAINS):
-                models.append(load_model("%s/net_1/%s" % (CIFAR_100_DIR, TRAIN_FILENAME % train_idx), custom_objects={"backend": backend}))
-        else:
-            for train_idx in xrange(NUM_TRAINS*2):
-                if train_idx < 5:
-                    models.append(load_model("%s/net_1/%s" % (CIFAR_100_DIR, TRAIN_FILENAME % train_idx), custom_objects={"backend": backend}))
-                else:
-                    models.append(load_model("%s/net_9/%s" % (CIFAR_100_DIR, TRAIN_FILENAME % train_idx), custom_objects={"backend": backend}))
+    else:
+        for train_idx in xrange(NUM_TRAINS*2):
+            print 'loading', train_idx
+            if train_idx < 5:
+                models.append(load_model("%s/net_1/%s" % (TRAIN_DIR, TRAIN_FILENAME % train_idx), custom_objects={"backend": backend}))
+            else:
+                models.append(load_model("%s/net_9/%s" % (TRAIN_DIR, TRAIN_FILENAME % (train_idx-NUM_TRAINS)), custom_objects={"backend": backend}))
+
 
 
     for model in models:
+        print 'predicting...'
         for _ in range(AUGMENT_TEST):
             x_test_augmented = np.array([augmentation(image) for image in dataset['x_test']])
             predictions.append(model.predict(x_test_augmented, batch_size=125, verbose=2))
@@ -91,9 +108,8 @@ def test(n_classes=10, one_model=True):
     print accuracy
 
 NUM_TRAINS = 5
+
 AUGMENT_TEST = 100
-CIFAR_10_DIR = './CIFAR-10/'
-CIFAR_100_DIR = './CIFAR-100/'
 TRAIN_FILENAME = 'train_%d.hdf5'
 
 if __name__ == '__main__':
@@ -101,22 +117,46 @@ if __name__ == '__main__':
                                                              'multiple',
                                                             ])
 
-    dataset = None
     multiple = True
+    DATASET = None
     
     for opt, arg in options:
         if opt in ('-d', '--dataset'):
             if arg == 'cifar-10':
-                dataset = 10
+                TRAIN_DIR = './CIFAR-10/'
+                DATASET = 'cifar-10'
+                NUM_CLASSES = 10
+                WIDTH = 32
+                HEIGHT = 32
+                NUM_CHANNELS = 3
             elif arg == 'cifar-100':
-                dataset = 100
+                TRAIN_DIR = './CIFAR-100/'
+                DATASET = 'cifar-100'
+                NUM_CLASSES = 100
+                WIDTH = 32
+                HEIGHT = 32
+                NUM_CHANNELS = 3
+            elif arg == 'mnist':
+                TRAIN_DIR = './MNIST/'
+                DATASET = 'mnist'
+                NUM_CLASSES = 10
+                WIDTH = 28
+                HEIGHT = 28
+                NUM_CHANNELS = 1
+            elif arg == 'fashion-mnist':
+                TRAIN_DIR = './FASHION-MNIST/'
+                DATASET = 'fashion-mnist'
+                NUM_CLASSES = 10
+                WIDTH = 28
+                HEIGHT = 28
+                NUM_CHANNELS = 1
 
         if opt in ('-m', '--multiple'):
-            one_model = False
+            multiple = False
 
-    if dataset is None:
+    if DATASET is None:
         print 'Invalid dataset!'
 
     else:
-        test(dataset, multiple)
+        test(multiple)
 
